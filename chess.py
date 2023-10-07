6f79aeeaa7841758981e496a1a84ddc1e1a3613a
MasterCrafter69 <111368986+MasterCrafter69@users.noreply.github.com>
2023-10-06 18:48:16 -0600

    Add files via upload

--- chess.py
+++ chess.py
@@ -2,62 +2,68 @@ import pygame
 import sys
 
 # Función para calcular el valor basado en piezas capturadas
-def value(pawn, knight, bishop, rook, queen):
-    # Devuelve el valor total basado en la suma de piezas capturadas y sus respectivos valores
-    return pawn + 3 * (knight + bishop) + 5 * rook + 9 * queen
+def value(pieces):
+    values = {'pawn': 1, 'knight': 3, 'bishop': 3, 'rook': 5, 'queen': 9}
+    return sum(values[piece] * count for piece, count in pieces.items())
 
 def main():
-    # Solicita al usuario que introduzca la cantidad de piezas capturadas de cada tipo
-    pawn = int(input("how many pawns did you capture?"))
-    knight = int(input("how many knights did you capture?"))
-    bishop = int(input("how many bishops did you capture?"))
-    rook = int(input("how many rooks did you capture?"))
-    queen = int(input("how many queens did you capture?"))
+    # Usamos un diccionario para almacenar las piezas y evitar repetir código
+    pieces = {
+        'pawn': 0,
+        'knight': 0,
+        'bishop': 0,
+        'rook': 0,
+        'queen': 0
+    }
+
+    for piece in pieces.keys():
+        pieces[piece] = int(input(f"how many {piece}s did you capture?"))
+
     # Muestra la puntuación calculada
-    print("your score is:", value(pawn, knight, bishop, rook, queen))
+    print(f"your score is: {value(pieces)}")
 
 # Configuraciones iniciales de pygame
-ANCHO, ALTO = 640, 640  # Dimensiones de la ventana
-FILAS, COLUMNAS = 8, 8  # Dimensiones del tablero
-TAMAÑO_CASILLA = ANCHO // COLUMNAS  # Tamaño de cada casilla del tablero
+ANCHO, ALTO = 640, 640
+FILAS, COLUMNAS = 8, 8
+TAMAÑO_CASILLA = ANCHO // COLUMNAS
 
 # Colores utilizados
 BLANCO = (255, 255, 255)
 NEGRO = (0, 0, 0)
 
+# Matriz para representar el tablero
+# Por ahora está vacío, pero puedes poner piezas en él
+# Ejemplo: tablero[0][0] = 'pawn' representa un peón en la esquina superior izquierda
+tablero = [[None for _ in range(COLUMNAS)] for _ in range(FILAS)]
+
 def dibuja_tablero():
-    # Dibuja un tablero de ajedrez en la ventana
-    ventana.fill(BLANCO)  # Establece el fondo blanco
+    ventana.fill(BLANCO)
     for fila in range(FILAS):
-        # Determina el color inicial de la fila
         color_actual = BLANCO if fila % 2 == 0 else NEGRO
         for col in range(COLUMNAS):
             pygame.draw.rect(ventana, color_actual, (col * TAMAÑO_CASILLA, fila * TAMAÑO_CASILLA, TAMAÑO_CASILLA, TAMAÑO_CASILLA))
-            # Alterna el color para la siguiente casilla
             color_actual = NEGRO if color_actual == BLANCO else BLANCO
-    pygame.display.flip()  # Actualiza la ventana
+            # Si hay una pieza en la casilla, la dibujas (aquí solo es un ejemplo de cómo hacerlo)
+            if tablero[fila][col]:
+                piece_text = font.render(tablero[fila][col], True, (255,0,0))  # Renderiza el nombre de la pieza como texto
+                ventana.blit(piece_text, (col * TAMAÑO_CASILLA + TAMAÑO_CASILLA // 4, fila * TAMAÑO_CASILLA + TAMAÑO_CASILLA // 4))
 
-# Inicializa pygame
+    pygame.display.flip()
+
 pygame.init()
-
-# Crea la ventana del juego
 ventana = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption("Tablero con Pygame")
+font = pygame.font.SysFont(None, 36)  # Fuente para dibujar texto (piezas en este caso)
 
-# Dibuja el tablero inicial
 dibuja_tablero()
 
-# Bucle principal para manejar eventos
 corriendo = True
 while corriendo:
     for evento in pygame.event.get():
-        # Si el usuario cierra la ventana, finaliza el bucle
         if evento.type == pygame.QUIT:
             corriendo = False
             break
 
-# Finaliza pygame
 pygame.quit()
-
-# Llama a la función main para que el usuario introduzca las piezas capturadas y muestre el resultado
 main()
+
