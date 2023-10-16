49ade8f5163ad32ce0ccf895fe0e588ee4524b6e
MasterCrafter69 <111368986+MasterCrafter69@users.noreply.github.com>
2023-10-15 23:46:03 -0600

    Add files via upload

--- chess.py
+++ chess.py
@@ -1,4 +1,8 @@
-# Funciones de menú
+# Chess PGN Recorder
+# Author: Daniel Aguilar Darío
+# Description: This program allows users to record and save chess games in PGN format.
+
+# Menu functions
 def menu1():
     print("1. Create game")
     print("2. Information")
@@ -8,10 +12,15 @@ def menu2():
     print("1. Record game moves")
     print("2. Add game information")
     print("3. Statistics")
-    print("4. Create .pgn file")
-    print("5. Return to main menu")
+    print("4. Add comments to moves")
+    print("5. Create .PGN file")
+    print("6. Return to main menu")
 
-# Funciones previamente definidas
+
+
+# Input: None
+# Process: Asks user for specific game details
+# Output: Returns a list of tags with corresponding values
 def tags():
     roster = ["Event", "Site", "Date", "Round", "White", "Black", "Result"]
     str_matrix = []
@@ -22,34 +31,40 @@ def tags():
     
     return str_matrix
 
+# Input: Existing game moves
+# Process: Records the moves made by white and black players
+# Output: Updated list of moves
 def moves(game):
     move_number = 1
-    terminaciones = ['#', '1/2-1/2', '*']
-    
-    while True:
-        # Solicita la jugada de las blancas
-        white_move = input(f"Jugada {move_number} Blancas: ")
+    terminations = ['#', '1/2-1/2', '*']
 
-        # Si el usuario presiona "Enter" sin ingresar una jugada o ingresa una terminación, terminamos el bucle
-        if not white_move or any(terminacion in white_move for terminacion in terminaciones):
+    while True:
+        # Prompt the move for white
+        white_move = input(f"Move {move_number} White: ")
+
+        # If the user presses "Enter" without entering a move or enters a termination, exit the loop
+        if not white_move or any(termination in white_move for termination in terminations):
             game.append((white_move,))
             break
 
-        # Solicita la jugada de las negras
-        black_move = input(f"Jugada {move_number} Negras: ")
+        # Prompt the move for black
+        black_move = input(f"Move {move_number} Black: ")
 
-        # Si detecta una terminación en la jugada de las negras, terminamos el bucle
-        if any(terminacion in black_move for terminacion in terminaciones):
+        # If a termination is detected in black's move, exit the loop
+        if any(termination in black_move for termination in terminations):
             game.append((white_move, black_move))
             break
 
-        # Añade las jugadas a la matriz game
+        # Add the moves to the game matrix
         game.append((white_move, black_move))
-        
+
         move_number += 1
-        
+
     return game
-        
+
+# Input: Game moves
+# Process: Calculates the number of different types of moves made by both players
+# Output: Displays the statistics for both players
 def statistics(game):
     notations = {
         '+': 'Check',
@@ -81,6 +96,9 @@ def statistics(game):
     for notation, description in notations.items():
         print(f"{description}: {black_statistics[notation]}")
 
+# Input: Existing game moves
+# Process: Allows user to add comments to specific moves
+# Output: Updated list of moves with comments
 def comments(game):
     move_num = int(input("Enter the move number you want to comment on: "))
     if move_num > len(game) or move_num <= 0:
@@ -105,7 +123,10 @@ def comments(game):
         else:
             print("There's no black move for this turn to comment on!")
 
-def pgn(str_matrix, game):
+# Input: List of tags and game moves
+# Process: Displays the game in PGN format and offers to save it to a .pgn file
+# Output: Saves the game to a .pgn file if user agrees
+def PGN(str_matrix, game):
     for tag, value in str_matrix:
         print(f"[{tag} \"{value}\"]")
     print()
@@ -114,10 +135,10 @@ def pgn(str_matrix, game):
         print(f"{index}. {white} {' '.join(black)}", end=" ")
     print("\n")
 
-    save_file = input("Do you want to save it as a .pgn file? (yes/no): ").lower()
+    save_file = input("Do you want to save it as a .PGN file? (yes/no): ").lower()
     if save_file == 'yes':
-        file_name = input("Enter the name for the .pgn file (without extension): ")
-        with open(file_name + ".pgn", "w") as file:
+        file_name = input("Enter the name for the .PGN file (without extension): ")
+        with open(file_name + ".PGN", "w") as file:
             for tag, value in str_matrix:
                 file.write(f"[{tag} \"{value}\"]\n")
             file.write("\n")
@@ -125,20 +146,29 @@ def pgn(str_matrix, game):
             for index, (white, *black) in enumerate(game, start=1):
                 file.write(f"{index}. {white} {' '.join(black)} ")
             file.write("\n")
-        print(f"PGN saved as {file_name}.pgn!")
+        print(f"PGN saved as {file_name}.PGN!")
 
-# Función principal
+# Read and display the content of the info.txt file
+def read_info():
+    try:
+        with open("info.txt", "r") as file:
+            content = file.read()
+            print(content)
+    except FileNotFoundError:
+        print("Error: info.txt file not found!")
+
+# Main function
 def main():
     str_matrix = []
     game = []
     
     while True:
         menu1()
-        choice1 = input("Choose an option: ")
+        choice1 = input("Choose an option: ").replace("png", "PNG")
         if choice1 == "1":
             while True:
                 menu2()
-                choice2 = input("Choose an option: ")
+                choice2 = input("Choose an option: ").replace("png", "PNG")
                 if choice2 == "1":
                     moves(game)
                 elif choice2 == "2":
@@ -146,13 +176,15 @@ def main():
                 elif choice2 == "3":
                     statistics(game)
                 elif choice2 == "4":
-                    pgn(str_matrix, game)
+                    comments(game)
                 elif choice2 == "5":
+                    PGN(str_matrix, game)
+                elif choice2 == "6":
                     break
                 else:
                     print("Invalid option, try again.")
         elif choice1 == "2":
-            print("This program allows you to record and save chess games in PGN format.")
+            read_info()
         elif choice1 == "3":
             print("Exiting the program...")
             break
